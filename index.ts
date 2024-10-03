c8194141548ec648530eb24746aaf6a533444fa4
Tran Van Cong <aanh7108@gmail.com>
2024-10-03 23:39:29 +0700

    nhung file tinh

--- index.ts
+++ index.ts
@@ -3,8 +3,13 @@ import express, { Express } from "express";
 const app: Express = express();
 const port: number = 3000;
 
+app.set("views", "./views"); // dinh nghia thu muc view
+app.set("view engine", "pug"); // dinh nghia pug
+
+app.use(express.static("public")); // thu muc tinh
+
 app.get("/tour", (req, res) => {
-    res.send("ok");
+    res.render("client/page/tour");
 });
 
 app.listen(port, () => {
