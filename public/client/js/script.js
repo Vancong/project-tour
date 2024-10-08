c18543916321927ec8b9ce3a79cad424541d486f
Tran Van Cong <aanh7108@gmail.com>
2024-10-08 14:16:27 +0700

    lam sile cho anh

--- public/client/js/script.js
+++ public/client/js/script.js
@@ -1,1 +1,17 @@
-console.log("ok");
+//sile
+const tourImage = document.querySelector('.tour-image');
+if (tourImage) {
+    console.log(tourImage);
+    const swiper = new Swiper(".tour-image", {
+        cssMode: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+        },
+        mousewheel: true,
+        keyboard: true,
+    });
+}
