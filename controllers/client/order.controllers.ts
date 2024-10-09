09a2ec391889381bedb4d3e56fd4c9277acc25ca
Tran Van Cong <aanh7108@gmail.com>
2024-10-09 23:01:54 +0700

    2.3. Server lấy data lưu vào bảng orders_item

--- controllers/client/order.controllers.ts
+++ controllers/client/order.controllers.ts
@@ -1,20 +1,24 @@
 import { Request, Response } from "express";
 import orderDtb from "../../models/order.models";
 import { generateOrderCode } from "../../helpers/generate.helpers";
+import orderItemDtb from "../../models/order-item.models";
+import tourDtb from "../../models/tour.models";
 export const index = async (req: Request, res: Response) => {
     const data = req.body;
 
-    const dataOrder = {
+    // Lưu data vào bảng orders
+    const dataOrders = {
         code: "",
         fullName: data.info.fullName,
         phone: data.info.phone,
         note: data.info.note,
         status: "initial",
     };
-    const order = await orderDtb.create(dataOrder);
+
+    const order = await orderDtb.create(dataOrders);
     const orderId = order.dataValues.id;
     const code = generateOrderCode(orderId);
-    console.log(order);
+
     await orderDtb.update(
         {
             code: code,
@@ -26,9 +30,29 @@ export const index = async (req: Request, res: Response) => {
         }
     );
 
+    // Lưu data vào bảng orders_item
+    for (const item of data.cart) {
+        const dataItem = {
+            order_id: orderId,
+            tour_id: item.tourId,
+            quantity: item.quantity,
+        };
+        const tourInfo = await tourDtb.findOne({
+            where: {
+                id: item.tourId,
+                deleted: false,
+                status: "active",
+            },
+            raw: true,
+        });
+        dataItem["price"] = tourInfo["price"];
+        dataItem["discount"] = tourInfo["discount"];
+        dataItem["timeStart"] = tourInfo["timeStart"];
+        await orderItemDtb.create(dataItem);
+    }
+
     res.json({
         code: 200,
-        message: "thanh cong",
-        orderCode: code,
+        message: "Đặt hàng thành công!",
     });
 };
