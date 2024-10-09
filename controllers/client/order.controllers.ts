ac2f210dc99be3fd94808c35c23ae1ecc47a955a
Tran Van Cong <aanh7108@gmail.com>
2024-10-09 22:38:44 +0700

    2.2. Server lấy data lưu vào bảng orders

--- controllers/client/order.controllers.ts
+++ controllers/client/order.controllers.ts
@@ -1,11 +1,34 @@
 import { Request, Response } from "express";
-
+import orderDtb from "../../models/order.models";
+import { generateOrderCode } from "../../helpers/generate.helpers";
 export const index = async (req: Request, res: Response) => {
     const data = req.body;
-    console.log(data);
+
+    const dataOrder = {
+        code: "",
+        fullName: data.info.fullName,
+        phone: data.info.phone,
+        note: data.info.note,
+        status: "initial",
+    };
+    const order = await orderDtb.create(dataOrder);
+    const orderId = order.dataValues.id;
+    const code = generateOrderCode(orderId);
+    console.log(order);
+    await orderDtb.update(
+        {
+            code: code,
+        },
+        {
+            where: {
+                id: orderId,
+            },
+        }
+    );
 
     res.json({
         code: 200,
         message: "thanh cong",
+        orderCode: code,
     });
 };
